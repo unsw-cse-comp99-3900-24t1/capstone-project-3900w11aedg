44730b2c2e6af093a05a1370a95ed119e8a7c544
Christian Tolentino <chriscla2003@gmail.com>
2024-06-11 23:27:01 +1000

    Added lint fix command

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    '@react-native',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  plugins: ['@typescript-eslint'],
+  extends: ['@react-native', 'plugin:@typescript-eslint/recommended'],
 };
--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   preset: 'react-native',
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['./jest.setup.js']
+  setupFilesAfterEnv: ['./jest.setup.js'],
 };
-
