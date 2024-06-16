b16d877bbae25e87a519c2e601f586360fed1df6
Christian Tolentino <chriscla2003@gmail.com>
2024-06-16 21:01:23 +1000

    Renamed test file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,19 @@
 import express, {Request, Response} from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors);
+app.use(express.json());
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello, world!');
