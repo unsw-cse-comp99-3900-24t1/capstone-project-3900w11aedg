7e3f8832abffd17c8387832a6098f2cf165a28c8
Christian Tolentino <chriscla2003@gmail.com>
2024-06-24 18:40:50 +1000

    Enraged

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,7 +3,4 @@ module.exports = {
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['./jest.setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
-  ],
 };
