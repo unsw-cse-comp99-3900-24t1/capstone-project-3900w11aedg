9381d65964cfaa4d3422a285bbb8699ebc01817f
Angela Shan <z5358441@ad.unsw.edu.au>
2024-06-24 16:15:53 +1000

    added libraries directory with generate-did helper

---
+++ libraries/src/generate-did.ts
@@ -0,0 +1,30 @@
+import crypto from 'crypto';
+import axios from 'axios';
+import { DIDDocument } from 'did-resolver';
+
+export function generateDID(did: string, publicKey: string) {
+    const didHash = crypto.createHash('sha256');
+    didHash.update(did);
+    const didHashHex = didHash.digest('hex');
+    
+    const didDocument: DIDDocument = {
+        '@context': 'https://www.w3.org/ns/did/v1',
+        id: `did:web:${didHashHex}`,
+        publicKey: [
+        {
+            id: `did:web:${didHashHex}#key-1`,
+            type: 'Ed25519VerificationKey2018',
+            controller: `did:example:${didHashHex}`,
+            publicKeyBase58: publicKey,
+        },
+        ],
+    };
+
+    try {
+        axios.post(`http://localhost:5000/${didHashHex}/.well-known/did.json`, didDocument);
+    } catch (error) {
+        throw new Error('Error saving the did document');
+    }
+
+    return didDocument;
+}
