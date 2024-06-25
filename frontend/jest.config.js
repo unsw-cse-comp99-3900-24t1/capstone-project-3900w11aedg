997be45710bce138defd2df34d0bb4dfdb93bdb1
Christian Tolentino <chriscla2003@gmail.com>
2024-06-26 01:35:27 +1000

    No longer using stupid library

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['./jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-rsa-native)/)',
+  ],
 };
