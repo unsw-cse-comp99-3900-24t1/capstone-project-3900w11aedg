7f9794b9891f61ba15d8465910781d155da5a8f2
Angela Shan <z5358441@ad.unsw.edu.au>
2024-06-25 23:23:32 +1000

    modified tests to support route changes

--- service-provider/__tests__/request-credentials.test.ts
+++ service-provider/__tests__/request-credentials.test.ts
@@ -3,38 +3,66 @@ import app from '../src/index';
 import { server } from '../src';
 import { afterEach, describe, expect, it } from '@jest/globals';
 
-const claims: Record<string, string | string[]> = { 'Driver License': ['age', 'name'] };
+const claims = {
+  "id": "vp token example",
+  "input_descriptors": [
+    {
+      "id": "id card credential",
+      "format": {
+        "ldp_vc": {
+          "proof_type": [
+            "Ed25519Signature2018"
+          ]
+        }
+      },
+      "constraints": {
+        "fields": [
+          {
+            "path": [
+              "$.type"
+            ],
+            "filter": {
+              "type": "string",
+              "pattern": "IDCardCredential"
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
 const serviceProviderDID = 'did:web:example.com';
+const domain = 'localhost';
 
-describe('POST /credential/request', () => {
+describe('POST /generate/qr-code', () => {
   afterEach(async () => {
     await server.close();
   });
 
   it('should respond with a valid status and a QR code URL', async () => {
     const response = await request(app)
-      .post('/credential/request')
-      .send({ claims, serviceProviderDID });
+      .post('/generate/qr-code')
+      .send({ claims, serviceProviderDID, domain });
 
     expect(response.status).toBe(200);
-    expect(response.body.qrCode).toMatch(/^data:image\/png;base64,/);
+    expect(response.body.qrCodeUrl).toMatch(/^data:image\/png;base64,/);
   });
 
-  it('should respond with status 404 when DID is invalid', async () => {
+  it('should respond with status 400 when DID is invalid', async () => {
     const response = await request(app)
-      .post('/credential/request')
-      .send({ claims, serviceProviderDID: 'did:example:invalid' });
-
-    expect(response.status).toBe(404);
-    expect(response.text).toBe('Service Provider DID not found!');
-  });
-
-  it('should respond with status 400 when claims are not provided', async () => {
-    const response = await request(app)
-      .post('/credential/request')
-      .send({ claims: {}, serviceProviderDID });
+      .post('/generate/qr-code')
+      .send({ claims, serviceProviderDID: 'did:example:invalid', domain});
 
     expect(response.status).toBe(400);
-    expect(response.text).toBe('Invalid parameters');
+    expect(response.text).toBe('Invalid Service Provider DID');
+  });
+
+  it('should respond with status 400 when claims are of invalid structure', async () => {
+    const response = await request(app)
+      .post('/generate/qr-code')
+      .send({ claims: {"test": "structure"}, serviceProviderDID, domain });
+
+    expect(response.status).toBe(400);
+    expect(response.text).toBe('Invalid claims');
   });
 });
