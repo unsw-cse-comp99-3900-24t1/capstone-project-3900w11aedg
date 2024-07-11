deb09d5423c66faa10987a39c52ca56f6968fdfb
Alan Nguyen <aland.nguyen24@gmail.com>
2024-07-11 13:40:21 +1000

    Creating commit for general theme frontend structure changes

--- frontend/src/components/ScanQR.tsx
+++ frontend/src/components/ScanQR.tsx
@@ -1,12 +1,21 @@
-import { View } from 'react-native';
+import { Image, Dimensions } from 'react-native';
 import React from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 function ScanQR(): JSX.Element {
   return (
-    <View className="w-[80%] h-[80%]">
-      <QRCodeScanner onRead={() => console.log('scanned')} />
-    </View>
+    <QRCodeScanner
+      onRead={() => console.log('scanned')}
+      showMarker={true}
+      customMarker={
+        <Image
+          className="h-[65%]"
+          source={require('../assets/scan_marker.png')}
+          resizeMode="contain"
+        />
+      }
+      cameraStyle={{ height: Dimensions.get('window').height / 2 }}
+    />
   );
 }
 
